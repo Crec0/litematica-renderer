4b40c70e541c18bd25f47da8fa18f60098b2a3d4
Crec0 <83436716+Crec0@users.noreply.github.com>
2023-01-28 19:31:10 -0800

    Add basic Viewer

---
+++ src/lib/Viewer.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import type { BlockDefinition, BlockModel, TextureAtlas } from "deepslate";
+    import { onMount } from "svelte";
+
+    export let blockDefinitions: Record<string, BlockDefinition>;
+    export let models: Record<string, BlockModel>;
+    export let textureAtlas: TextureAtlas;
+
+    let canvasElement: HTMLCanvasElement;
+
+    onMount(() => {
+        const ctx = canvasElement.getContext("webgl");
+    });
+</script>
+
+<canvas bind:this={canvasElement} class="border-4 w-screen h-screen" />
